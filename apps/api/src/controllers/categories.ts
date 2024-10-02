268253d8fa2138a42271c86ce25379ec5b93e3b4
amonzalvo <amonzalvo.laboral@outlook.com>
2024-10-02 16:32:42 -0600

    (MOD) Refactor category code from no-pattern to MVC

---
+++ apps/api/src/controllers/categories.ts
@@ -0,0 +1,74 @@
+import { Category } from '../models/category'
+
+//Variables
+const categories: Array<Category> = []
+let id = 0
+
+//Functions
+export function categoryById(req): number {
+    const {id} = req.params
+    return categories.findIndex( item => item.id == id)
+  }
+
+//GET all categories
+export const getCategories = (req,res) => {
+    res.status(200).json(categories)
+}
+
+//GET a specific category by ID
+export const getCategory = (req,res) => {
+    const index = categoryById(req)
+  if(index > -1){
+    res.status(200).json(categories[index])
+  } else {
+    res.status(404).json({message: 'Category not found'})
+  }
+}
+
+//POST create a category
+export const createCategory = (req, res) => {
+    const {name} = req.body
+    if(name){
+      const category: Category = {
+        id,
+        name
+      }
+      categories.push(category)
+      res.status(201).json(category)
+      id += 1
+    } else {
+      return res.status(400).json({message: 'Name is required'})
+    }
+}
+
+//PATCH update a specific category by ID
+export const updateCategory = (req, res) => {
+    const {name} = req.body
+    const index = categoryById(req)
+    if(name && index > -1){
+      const category: Category = categories[index]
+      category.name = name
+      categories[index] = category
+      res.status(200).json(category)
+      id += 1
+    } else {
+      if(index < 0){
+        return res.status(404).json({message: 'Not found'})
+      }
+      if(!name){
+        return res.status(400).json({message: 'Name is required'})
+      }
+    }
+}
+
+//DELETE delete a especific category by ID
+export const deleteCategory = (req, res) => {
+    const index = categoryById(req)
+    if(index > -1){
+      categories.splice(index,1)
+      res.status(204).send()
+      id += 1
+    } else {
+        return res.status(404).json({message: 'Not found'})
+    }
+}
