2e58d965e1a44f69fdc900b44173ecb5f00a1e95
amonzalvo <amonzalvo.laboral@outlook.com>
2024-10-09 11:36:50 -0600

    (ADD) Add interface, schema and model of Category information

--- apps/api/src/models/category.ts
+++ apps/api/src/models/category.ts
@@ -1,5 +1,23 @@
+import mongoose,{Document, Schema} from "mongoose"
+
 export class Category {
     id: number = 0
     name: string = ''
     constructor(){}
 }
+export interface ICategory extends Document {
+    name: string
+}
+
+export const CategorySchema = new Schema<ICategory>(
+    {
+        name: {
+            type: String,
+            required: [true, 'Property is required']
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+export const CategoryM = mongoose.model<ICategory>('Category', CategorySchema)
