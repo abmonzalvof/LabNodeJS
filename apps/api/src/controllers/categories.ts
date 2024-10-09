8dcc6c3e3003672e62971da25cf076612af19612
amonzalvo <amonzalvo.laboral@outlook.com>
2024-10-09 11:38:52 -0600

    (MOD) Refactor functions to manage data in DB

--- apps/api/src/controllers/categories.ts
+++ apps/api/src/controllers/categories.ts
@@ -1,74 +1,72 @@
-import { Category } from '../models/category'
+import { CategoryM } from '../models/category'
 
-//Variables
-const categories: Array<Category> = []
-let id = 0
-
-//Functions
-function categoryById(req): number {
-  const {id} = req.params
-  return categories.findIndex( item => item.id == id)
-}
 
 //GET all categories
-export const getCategories = (req,res) => {
+export const getCategories = async (req,res) => {
+  try{
+    const categories = await CategoryM.find()
     res.status(200).json(categories)
-}
-
-//GET a specific category by ID
-export const getCategory = (req,res) => {
-    const index = categoryById(req)
-  if(index > -1){
-    res.status(200).json(categories[index])
-  } else {
-    res.status(404).json({message: 'Category not found'})
+  }catch(e){
+    const {message} = e
+    res.status(500).json({message})
   }
 }
 
+//GET a specific category by ID
+export const getCategory = async (req,res) => {
+    try{
+      const {id} = req.params
+      const category = await CategoryM.findById(id)
+      res.status(200).json(category)
+    }catch(e){
+      const {message} = e
+      res.status(500).json({message})
+    }
+}
+
 //POST create a category
-export const createCategory = (req, res) => {
+export const createCategory = async (req, res) => {
     const {name} = req.body
     if(name){
-      const category: Category = {
-        id,
-        name
+      try {
+        const category = await CategoryM.create(req.body)
+        res.status(201).json(category)
+      } catch (error) {
+        const {message} = error
+          res.status(500).json({message})
       }
-      categories.push(category)
-      res.status(201).json(category)
-      id += 1
     } else {
       return res.status(400).json({message: 'Name is required'})
     }
 }
 
 //PATCH update a specific category by ID
-export const updateCategory = (req, res) => {
-    const {name} = req.body
-    const index = categoryById(req)
-    if(name && index > -1){
-      const category: Category = categories[index]
-      category.name = name
-      categories[index] = category
+export const updateCategory = async (req, res) => {
+    try {
+      const {id} = req.params
+      const category = await CategoryM.findByIdAndUpdate(id, req.body, {new: true})
+      if(!category){
+        return res.status(401).json({message: 'Not found'})
+      }
       res.status(200).json(category)
-      id += 1
-    } else {
-      if(index < 0){
-        return res.status(404).json({message: 'Not found'})
-      }
-      if(!name){
-        return res.status(400).json({message: 'Name is required'})
-      }
+      
+    } catch (error) {
+        const {message} = error
+        res.status(500).json({message})
     }
 }
 
 //DELETE delete a especific category by ID
-export const deleteCategory = (req, res) => {
-    const index = categoryById(req)
-    if(index > -1){
-      categories.splice(index,1)
-      res.status(204).send()
-      id += 1
-    } else {
-        return res.status(404).json({message: 'Not found'})
+export const deleteCategory = async (req, res) => {
+  try {
+    const {id} = req.params
+    const category = await CategoryM.findByIdAndDelete(id)
+    if(!category){
+      return res.status(401).json({message: 'Not found'})
     }
+      res.status(204).send()
+  } catch (error) {
+    const {message} = error
+    res.status(500).json({message})
+  }
 }
